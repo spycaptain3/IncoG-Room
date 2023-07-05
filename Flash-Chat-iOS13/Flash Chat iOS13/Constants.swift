6a88f763fdfd3b5e05d868cff0c09bf4a2290437
Deepak Gupta <spycaptain3@gmail.com>
2023-07-05 17:40:09 +0530

    Incogroom

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Created by Deepak Gupta on 17/12/22.
+//
+
+struct K {
+    
+    static let appName = "🎭IncoG-Room"
+
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
